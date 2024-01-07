4340828a8daa3dd45e31198e5f56e641170ad8b9
cristian <nem.cristianrc@gmail.com>
2024-01-07 10:50:33 -0300

    feat: entidade tools

--- src/main/java/api/vuttr/model/Tool.java
+++ src/main/java/api/vuttr/model/Tool.java
@@ -1,4 +1,92 @@
 package api.vuttr.model;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Tool {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String link;
+    private List<String> tags;
+
+    public Tool() {
+    }
+
+    public Tool(Long id, String title, String description, String link, List<String> tags) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.link = link;
+        this.tags = tags;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public void addTag(String tag) {
+        this.tags.add(tag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tool tool = (Tool) o;
+
+        if (!Objects.equals(id, tool.id)) return false;
+        if (!Objects.equals(title, tool.title)) return false;
+        if (!Objects.equals(description, tool.description)) return false;
+        if (!Objects.equals(link, tool.link)) return false;
+        return Objects.equals(tags, tool.tags);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (link != null ? link.hashCode() : 0);
+        result = 31 * result + (tags != null ? tags.hashCode() : 0);
+        return result;
+    }
 }
